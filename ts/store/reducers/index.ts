b94df230260e5eed12419bd4fae71d8d5eae006b
Gianluca Spada <gianluca.spada@spindox.it>
2024-05-16 18:08:15 +0200

    chore: [SIW-1099] Replicate basic project structure of `io-app` (#2)
    * chore: install additional libraries
    
    * chore: add i18n and utils from io-app
    
    * chore: configure and add redux, redux-saga, utils
    
    * chore: update Podfile.lock
    
    * chore: add package.json variables for definitions generation

---
+++ ts/store/reducers/index.ts
@@ -0,0 +1,42 @@
+/**
+ * Aggregates all defined reducers
+ */
+import { combineReducers, Reducer } from "redux";
+import { PersistConfig } from "redux-persist";
+import { Action } from "../actions/types";
+import appStateReducer from "./appState";
+import { navigationReducer } from "./navigation";
+import { GlobalState } from "./types";
+import { debugReducer } from "./debug";
+
+/**
+ * Here we combine all the reducers.
+ * We use the best practice of separating UI state from the DATA state.
+ * UI state is mostly used to check what to show hide in the screens (ex.
+ * errors/spinners).
+ * DATA state is where we store real data fetched from the API (ex.
+ * profile/messages).
+ * More at
+ * @https://medium.com/statuscode/dissecting-twitters-redux-store-d7280b62c6b1
+ */
+export const appReducer: Reducer<GlobalState, Action> = combineReducers<
+  GlobalState,
+  Action
+>({
+  //
+  // ephemeral state
+  //
+  appState: appStateReducer,
+  navigation: navigationReducer,
+
+  // standard persistor, see configureStoreAndPersistor.ts
+  debug: debugReducer
+});
+
+export function createRootReducer(
+  persistConfigs: ReadonlyArray<PersistConfig>
+) {
+  return (state: GlobalState | undefined, action: Action): GlobalState => {
+    return appReducer(state, action);
+  };
+}
---
+++ ts/utils/messages.ts
@@ -0,0 +1,1 @@
+export type ExpireStatus = "VALID" | "EXPIRING" | "EXPIRED";
