d4a2026302daf7ddf2e0482667e2fe31c811f328
Gianluca Spada <gianluca.spada@spindox.it>
2024-06-13 16:00:49 +0200

    chore: [SIW-1241] add packages required for IT wallet integration (#10)
    * chore: add io-react-native-wallet@0.11.1
    
    * chore: install required dependencies
    
    * chore: update react-native-webview
    
    * chore: remove io-react-native-cie
    
    * chore: rename kotlin_version to kotlinVersion
    
    * chore: add generic components used in it-wallet feature
    
    * Revert "chore: rename kotlin_version to kotlinVersion"
    
    This reverts commit 947f3e855c4091fe62fbf4742a60a0dbdc639972.

--- ts/navigation/AuthenticatedStackNavigator.tsx
+++ ts/navigation/AuthenticatedStackNavigator.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { H1, Body } from "@pagopa/io-app-design-system";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { PID } from "@pagopa/io-react-native-wallet";
+import cieManager from "@pagopa/io-react-native-cie-pid";
+import { ProximityManager } from "@pagopa/io-react-native-proximity";
 import { AppParamsList } from "./params/AppParamsList";
 import ROUTES from "./routes";
 
@@ -13,11 +15,20 @@ const hideHeaderOptions = {
 };
 
 const InitialScreen = () => {
+  const [hasNFCFeature, setHasNFCFeature] = useState(false);
+
+  useEffect(() => {
+    cieManager.hasNFCFeature().then(setHasNFCFeature);
+  }, []);
+
   return (
     <SafeAreaView>
       <H1>Initial screen</H1>
       <Body>React Native Application for EUDIW PoC</Body>
       <Body>IO React Native Wallet: {PID.SdJwt ? "✅" : "❗️"}</Body>
+      <Body>IO React Native CIE PID: {cieManager ? "✅" : "❗️"}</Body>
+      <Body>IO React Native Proximity: {ProximityManager ? "✅" : "❗️"}</Body>
+      <Body>NFC Available: {hasNFCFeature ? "✅" : "❗️"}</Body>
     </SafeAreaView>
   );
 };
