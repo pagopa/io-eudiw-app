e7f8d7113f594bdf25e52a0b760da582f51e8612
Sviluppo EUDIW <mario.perrotta@pagopa.it>
2024-06-13 16:00:49 +0200

    chore: [SIW-1241] add packages required for IT wallet integration (#10)
    * chore: add io-react-native-wallet@0.11.1
    
    * chore: install required dependencies
    
    * chore: update react-native-webview
    
    * chore: remove io-react-native-cie
    
    * chore: rename kotlin_version to kotlinVersion
    
    * chore: add generic components used in it-wallet feature
    
    * Revert "chore: rename kotlin_version to kotlinVersion"
    
    This reverts commit 947f3e855c4091fe62fbf4742a60a0dbdc639972.

---
+++ ts/components/animations/ScaleInOutAnimation.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { ViewStyle, StyleProp } from "react-native";
+// eslint-disable-next-line import/default
+import Animated, {
+  LayoutAnimation,
+  WithSpringConfig,
+  withDelay,
+  withSpring,
+  withTiming
+} from "react-native-reanimated";
+
+type Props = {
+  visible?: boolean;
+  springConfig?: WithSpringConfig;
+  delayOut?: number;
+  delayIn?: number;
+  children: React.ReactNode;
+  style?: StyleProp<Animated.AnimateStyle<StyleProp<ViewStyle>>>;
+};
+
+const ScaleInOutAnimation = ({
+  visible = true,
+  springConfig = { damping: 500, mass: 3, stiffness: 1000 },
+  delayOut = 0,
+  delayIn = 0,
+  children,
+  style
+}: Props) => {
+  const enteringAnimation = (): LayoutAnimation => {
+    "worklet";
+    return {
+      initialValues: {
+        transform: [{ scale: 0 }]
+      },
+      animations: {
+        transform: [{ scale: withDelay(delayIn, withSpring(1, springConfig)) }]
+      }
+    };
+  };
+
+  const exitingAnimation = (): LayoutAnimation => {
+    "worklet";
+    return {
+      initialValues: {
+        transform: [{ scale: 1 }]
+      },
+      animations: {
+        transform: [{ scale: withDelay(delayOut, withTiming(0)) }]
+      }
+    };
+  };
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <Animated.View
+      style={style}
+      entering={enteringAnimation}
+      exiting={exitingAnimation}
+    >
+      {children}
+    </Animated.View>
+  );
+};
+
+export { ScaleInOutAnimation };
