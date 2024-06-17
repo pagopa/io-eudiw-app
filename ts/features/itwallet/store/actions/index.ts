c5352c085e3c383306c2b1d5fb3d22dd656f0dde
Gianluca Spada <gianluca.spada@spindox.it>
2024-06-17 11:25:34 +0200

    feat: add all reducers and actions

--- ts/features/itwallet/store/actions/index.ts
+++ ts/features/itwallet/store/actions/index.ts
@@ -1,6 +1,29 @@
+import { ItwPersistedCredentialsActions } from "./itwPersistedCredentialsActions";
+import { ItwIssuancePidCieAuthActions } from "./itwIssuancePidCieActions";
 import { ItwActivationActions } from "./itwActivationActions";
+import { ItwWiaActions } from "./itwWiaActions";
 import { ItwLifecycleActions } from "./itwLifecycleActions";
+import { ItwRpActions } from "./itwPrRemotePidActions";
+import { itwPrRemoteCredentialInit } from "./itwPrRemoteCredentialActions";
+import { ItwIssuanceCredentialActions } from "./itwIssuanceCredentialActions";
+import { ItwProximityActions } from "./itwProximityActions";
+import { ItwIssuancePidActions } from "./itwIssuancePidActions";
+
 /**
  * Action types for the IT Wallet feature
  */
-export type ItWalletActions = ItwLifecycleActions | ItwActivationActions;
+export type ItWalletActions =
+  /* GENERIC */
+  | ItwLifecycleActions
+  | ItwWiaActions
+  | ItwActivationActions
+  /* ISSUANCE */
+  | ItwIssuancePidCieAuthActions
+  | ItwIssuancePidActions
+  | ItwIssuanceCredentialActions
+  /* PERSISTED CREDENTIALS */
+  | ItwPersistedCredentialsActions
+  /* PRESENTATION */
+  | ItwRpActions
+  | itwPrRemoteCredentialInit
+  | ItwProximityActions;
