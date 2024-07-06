49fa6da0dbe7f4fcf0f79ab0be5af6fefa8958c6
eudiwtech <eudiwtech@pagopa.it>
2024-07-06 16:21:22 +0200

    feat: [EUDIW-44]  Add onboarding carousel (#18)
    * feat: add wallet onboarding
    
    * chore: add en locales
    
    * fix: home redirect
    
    * chore: removed carousel tracking
    
    * chore: removed inline styles
    
    * deat: add reset onboarding state
    
    * chore: add redirect to app start on close
    
    * chore: align onboarding text to figma
    
    * chore: moved carousel to carousel screen
    
    ---------
    
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>

---
+++ ts/components/screens/OnboardingWallet/OnboardingWallet.tsx
@@ -0,0 +1,91 @@
+import React, { ComponentProps, useCallback } from "react";
+import { View } from "react-native";
+import {
+  ButtonLink,
+  ButtonSolid,
+  ContentWrapper,
+  IOColors
+} from "@pagopa/io-app-design-system";
+import I18n from "../../../i18n";
+import { useIONavigation } from "../../../navigation/params/AppParamsList";
+import ROUTES from "../../../navigation/routes";
+import { LandingCardComponent } from "../../LandingCardComponent";
+import { Carousel } from "../../../screens/carousel/Carousel";
+
+const MAIN_COLOR = "white";
+export const OnboardingWallet = () => {
+  const navigation = useIONavigation();
+
+  const skipCarousel = useCallback(() => {
+    navigation.navigate(ROUTES.ONBOARDING_WALLET_COMPLETE);
+  }, [navigation]);
+
+  const carouselCards: ReadonlyArray<
+    ComponentProps<typeof LandingCardComponent>
+  > = React.useMemo(
+    () => [
+      {
+        id: 0,
+        pictogramName: "cie", //TODO-> insert the right pictogram
+        title: I18n.t("features.itWallet.onboarding.card1.title"),
+        content: I18n.t("features.itWallet.onboarding.card1.content"),
+        accessibilityLabel: I18n.t("features.itWallet.onboarding.card1.title"),
+        accessibilityHint: I18n.t("features.itWallet.onboarding.card1.content"),
+        titleColor: MAIN_COLOR,
+        contentColor: MAIN_COLOR
+      },
+      {
+        id: 1,
+        pictogramName: "cie", //TODO-> insert the right pictogram
+        title: I18n.t("features.itWallet.onboarding.card2.title"),
+        content: I18n.t("features.itWallet.onboarding.card2.content"),
+        accessibilityLabel: I18n.t("features.itWallet.onboarding.card2.title"),
+        accessibilityHint: I18n.t("features.itWallet.onboarding.card2.content"),
+        titleColor: MAIN_COLOR,
+        contentColor: MAIN_COLOR
+      },
+      {
+        id: 2,
+        pictogramName: "cie", //TODO-> insert the right pictogram
+        title: I18n.t("features.itWallet.onboarding.card3.title"),
+        content: I18n.t("features.itWallet.onboarding.card3.content"),
+        accessibilityLabel: I18n.t("features.itWallet.onboarding.card3.title"),
+        accessibilityHint: I18n.t("features.itWallet.onboarding.card3.content"),
+        titleColor: MAIN_COLOR,
+        contentColor: MAIN_COLOR
+      }
+    ],
+    []
+  );
+  return (
+    <>
+      <ContentWrapper>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "flex-end"
+          }}
+        >
+          <ButtonLink
+            testID="skip-button-onboarding-wallet"
+            accessibilityLabel="features.itWallet.onboarding.skip"
+            color={"contrast"}
+            label={I18n.t("features.itWallet.onboarding.skip")}
+            onPress={skipCarousel}
+          />
+        </View>
+      </ContentWrapper>
+      <Carousel carouselCards={carouselCards} dotColor={IOColors.white} />
+      <ContentWrapper>
+        <ButtonSolid
+          testID="continue-button-onboarding-wallet"
+          accessibilityLabel="features.itWallet.onboarding.complete"
+          fullWidth={true}
+          color={"contrast"}
+          label={I18n.t("features.itWallet.onboarding.complete")}
+          onPress={skipCarousel}
+        />
+      </ContentWrapper>
+    </>
+  );
+};
