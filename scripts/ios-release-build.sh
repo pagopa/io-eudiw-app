7c73e708b3e59787396f7f1ec26c481f08d72baf
Mario Perrotta <mario.perrotta@pagopa.it>
2024-07-12 18:33:25 +0200

    feat: add android fastlane

--- scripts/ios-release-build.sh
+++ scripts/ios-release-build.sh
@@ -2,6 +2,6 @@
 
 cp .env.production .env
 yarn postinstall
-yarn cie-ios:ci
+# yarn cie-ios:ci
 touch ./ios/fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
 echo -e "$APP_STORE_API_PRIVATE_KEY" > ./ios/fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
