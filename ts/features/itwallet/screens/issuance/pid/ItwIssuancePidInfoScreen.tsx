1f29857ffd29ec57ad45b43fe5037445f80a5635
ITW Developers <lorenzo.curreli@spindox.it>
2024-08-06 14:30:55 +0200

    chore: update wallet activation first screen

--- ts/features/itwallet/screens/issuance/pid/ItwIssuancePidInfoScreen.tsx
+++ ts/features/itwallet/screens/issuance/pid/ItwIssuancePidInfoScreen.tsx
@@ -4,40 +4,25 @@ import { useNavigation } from "@react-navigation/core";
 import {
   ButtonSolidProps,
   VSpacer,
-  IOStyles
+  IOStyles,
+  ButtonSolid
 } from "@pagopa/io-app-design-system";
 import { SafeAreaView } from "react-native-safe-area-context";
 import I18n from "../../../../../i18n";
 import { ITW_ROUTES } from "../../../navigation/ItwRoutes";
-import { useIODispatch } from "../../../../../store/hooks";
-import { itwActivationStop } from "../../../store/actions/itwActivationActions";
 import ItwTextInfo from "../../../components/ItwTextInfo";
 import itwHeroImage from "../../../assets/img/issuing/itw_hero.png";
-import itwCardImage from "../../../assets/img/issuing/itw_card.png";
-import ItwFooterVerticalButtons from "../../../components/ItwFooterVerticalButtons";
 import { useHeaderSecondLevel } from "../../../../../hooks/useHeaderSecondLevel";
 import { IOStackNavigationProp } from "../../../../../navigation/params/AppParamsList";
 import { ItwParamsList } from "../../../navigation/ItwParamsList";
 
 const ItwIssuancePidInfoScreen = () => {
-  const dispatch = useIODispatch();
   const navigation = useNavigation<IOStackNavigationProp<ItwParamsList>>();
 
   useHeaderSecondLevel({
     title: "",
     supportRequest: true
   });
-
-  const bottomButtonProps: ButtonSolidProps = {
-    fullWidth: true,
-    color: "contrast",
-    label: I18n.t("features.itWallet.activationScreen.cancel"),
-    accessibilityLabel: I18n.t("features.itWallet.activationScreen.cancel"),
-    onPress: () => {
-      dispatch(itwActivationStop());
-    }
-  };
-
   const topButtonProps: ButtonSolidProps = {
     color: "primary",
     fullWidth: true,
@@ -70,14 +55,6 @@ const ItwIssuancePidInfoScreen = () => {
           />
           <VSpacer size={24} />
 
-          {/* Hero card image */}
-          <Image
-            source={itwCardImage}
-            style={{ width: "100%" }}
-            accessibilityIgnoresInvertColors
-          />
-          <VSpacer size={24} />
-
           {/* Info activation */}
           <ItwTextInfo
             content={I18n.t("features.itWallet.activationScreen.subContentTwo")}
@@ -85,10 +62,9 @@ const ItwIssuancePidInfoScreen = () => {
         </View>
       </ScrollView>
       <VSpacer size={24} />
-      <ItwFooterVerticalButtons
-        topButtonProps={topButtonProps}
-        bottomButtonProps={bottomButtonProps}
-      />
+      <View style={IOStyles.horizontalContentPadding}>
+        <ButtonSolid {...topButtonProps} />
+      </View>
     </SafeAreaView>
   );
 };
