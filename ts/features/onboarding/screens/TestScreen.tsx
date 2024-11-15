b096d09f51a6969133ae64051121d91015d9b072
LazyAfternoons <lazyafternoons@outlook.it>
2024-11-15 17:56:09 +0100

    chore: add props to screen to test typing

--- ts/features/onboarding/screens/TestScreen.tsx
+++ ts/features/onboarding/screens/TestScreen.tsx
@@ -1,9 +1,13 @@
 import {Pressable, ScrollView, Text} from 'react-native';
 import React from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {useAppDispatch} from '../../../store';
 import {onboardingSetIsComplete} from '../store/reducer';
+import {TabNavigatorParamsList} from '../../../navigation/TabNavigator';
 
-const TestScreen = () => {
+type Props = NativeStackScreenProps<TabNavigatorParamsList, 'HOME'>;
+
+const TestScreen = (_: Props) => {
   const dispatch = useAppDispatch();
 
   const onPress = () => {
