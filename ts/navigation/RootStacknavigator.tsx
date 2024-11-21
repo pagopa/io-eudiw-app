03d89eed3bf9eda4809144a92486ea637dec2317
LazyAfternoons <lazyafternoons@outlook.it>
2024-11-21 17:20:47 +0100

    refactor: use function to conditionally render screens

--- ts/navigation/RootStacknavigator.tsx
+++ ts/navigation/RootStacknavigator.tsx
@@ -1,6 +1,6 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {useIOThemeContext} from '@pagopa/io-app-design-system';
 import OnboardingNavigator from '../features/onboarding/navigation/OnboardingNavigator';
 import {useAppDispatch, useAppSelector} from '../store';
@@ -15,8 +15,16 @@ import {IONavigationDarkTheme, IONavigationLightTheme} from './theme';
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 /**
+ * Type definition for the screens to be rendered based on the startup and onboarding states.
+ */
+type Screens = {
+  name: keyof RootStackParamList;
+  component: React.ComponentType<any>;
+};
+
+/**
  * Entry point stack navigator for the application. This is the main navigation which orchestrates the whole app navigation.
- * It checks if the onboarding is not completed and shows the onboarding navigator, otherwise it shows the main tab navigator.
+ * It's based on the startup state and the onboarding completion state and renders the appropriate screen based on these states.
  */
 export const RootStackNavigator = () => {
   const isStartupDone = useAppSelector(selectStartupState);
@@ -28,40 +36,35 @@ export const RootStackNavigator = () => {
     dispatch(startupSetLoading());
   }, [dispatch]);
 
+  const getInitialScreen = useCallback((): Screens => {
+    switch (isStartupDone) {
+      case 'DONE':
+        // Startup is done, check if onboarding is completed
+        return isOnboardingCompleted
+          ? {name: ROUTES.MAIN_HOME, component: TabNavigation}
+          : {name: ONBOARDING_ROUTES.MAIN, component: OnboardingNavigator};
+      case 'ERROR':
+        // An error occurred during startup
+        return {name: ROUTES.MAIN_ERROR, component: () => <></>}; // TODO: Add error screen
+      case 'LOADING':
+      case 'NOT_STARTED':
+      default:
+        return {name: ROUTES.MAIN_LOADING, component: () => <></>}; // TODO: Add loading screen
+    }
+  }, [isStartupDone, isOnboardingCompleted]);
+
+  const initialScreen = getInitialScreen();
+
   return (
     <NavigationContainer
       theme={
         themeType === 'light' ? IONavigationLightTheme : IONavigationDarkTheme
       }>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        {
-          // Startup is done, render the home screen or onboarding navigator
-          isStartupDone === 'DONE' &&
-            (isOnboardingCompleted ? (
-              // Onboarding is completed, render the home screen
-              <Stack.Screen name={ROUTES.MAIN_HOME} component={TabNavigation} />
-            ) : (
-              // Onboarding not completed, render the onboarding navigator
-              <Stack.Screen
-                name={ONBOARDING_ROUTES.MAIN}
-                component={OnboardingNavigator}
-              />
-            ))
-        }
-
-        {
-          // Startup failed, render the error screen
-          isStartupDone === 'ERROR' && (
-            <Stack.Screen name={ROUTES.MAIN_ERROR} component={() => <></>} /> // TODO: Add error screen
-          )
-        }
-
-        {
-          // Startup is loading or not started, render the loading screen
-          (isStartupDone === 'LOADING' || isStartupDone === 'NOT_STARTED') && (
-            <Stack.Screen name={ROUTES.MAIN_LOADING} component={() => <></>} /> // TODO: Add loading screen
-          )
-        }
+        <Stack.Screen
+          name={initialScreen.name}
+          component={initialScreen.component}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
