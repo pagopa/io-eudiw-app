6a42743ae0ddc89d2eb15551a49ec3cbf31df7e4
eudiwtech <eudiwtech@pagopa.it>
2025-01-08 15:10:59 +0100

    fix: bump version and removal of unused env variables

--- scripts/android-release.sh
+++ scripts/android-release.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Recreate google-services.json from ENV variable
-echo $ENCODED_IOAPP_GOOGLE_SERVICES_JSON_FILE | base64 --decode > $1/google-services.json
-
 # Recreate JSON key file (for Google Play) from ENV variable
 echo $ENCODED_IOAPP_JSON_KEY_FILE | base64 --decode > /tmp/json-key.json
 
