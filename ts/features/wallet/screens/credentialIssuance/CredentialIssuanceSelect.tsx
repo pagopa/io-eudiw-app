8d1c943b8e683662e955c8baabe8c5025dce18ad
eudiwtech <eudiwtech@pagopa.it>
2025-02-07 15:41:05 +0100

    refactor : [WLEO-247] Ran prettier to make files compliant

--- ts/features/wallet/screens/credentialIssuance/CredentialIssuanceSelect.tsx
+++ ts/features/wallet/screens/credentialIssuance/CredentialIssuanceSelect.tsx
@@ -1,69 +1,84 @@
 import * as React from 'react';
-import { StyleSheet, View } from "react-native";
-import { useNavigation } from '@react-navigation/native';
-import { IOIcons, IOVisualCostants, ListItemHeader, ModuleCredential, VStack } from '@pagopa/io-app-design-system';
-import { useTranslation } from 'react-i18next';
-import { useHeaderSecondLevel } from '../../../../hooks/useHeaderSecondLevel';
-import { IOScrollViewWithLargeHeader } from '../../../../components/IOScrollViewWithLargeHeader';
-import { wellKnownCredential, getCredentialNameByType } from '../../utils/credentials';
+import {StyleSheet, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {
+  IOIcons,
+  IOVisualCostants,
+  ListItemHeader,
+  ModuleCredential,
+  VStack
+} from '@pagopa/io-app-design-system';
+import {useTranslation} from 'react-i18next';
+import {useHeaderSecondLevel} from '../../../../hooks/useHeaderSecondLevel';
+import {IOScrollViewWithLargeHeader} from '../../../../components/IOScrollViewWithLargeHeader';
+import {
+  wellKnownCredential,
+  getCredentialNameByType
+} from '../../utils/credentials';
 
-const availableCredentials = [wellKnownCredential.PID, wellKnownCredential.DRIVING_LICENSE];
+const availableCredentials = [
+  wellKnownCredential.PID,
+  wellKnownCredential.DRIVING_LICENSE
+];
 
-function getCredentialIconByType(type :string) : IOIcons | undefined {
-    switch (type) {
-        case wellKnownCredential.PID :
-            return 'fingerprint';
-        case wellKnownCredential.DRIVING_LICENSE :
-            return 'car';
-        default :
-            return undefined;
-    }
+function getCredentialIconByType(type: string): IOIcons | undefined {
+  switch (type) {
+    case wellKnownCredential.PID:
+      return 'fingerprint';
+    case wellKnownCredential.DRIVING_LICENSE:
+      return 'car';
+    default:
+      return undefined;
+  }
 }
 
 const CredentialIssuanceSelect = () => {
-    const navigation = useNavigation();
-    const {t} = useTranslation(['wallet']);
+  const navigation = useNavigation();
+  const {t} = useTranslation(['wallet']);
 
-    useHeaderSecondLevel({
-        title: '',
-        goBack: () => {
-        navigation.goBack();
-        },
-        supportRequest: true
-    });
+  useHeaderSecondLevel({
+    title: '',
+    goBack: () => {
+      navigation.goBack();
+    },
+    supportRequest: true
+  });
 
-    return (
-        <IOScrollViewWithLargeHeader
-            title={{
-                label : t('wallet:addcredential.choosecredentialtoadd.title')
-            }}
-        >
-            <View style={styles.wrapper}>
-                <ListItemHeader 
-                    label={t('wallet:addcredential.choosecredentialtoadd.headers.documents')}
+  return (
+    <IOScrollViewWithLargeHeader
+      title={{
+        label: t('wallet:addcredential.choosecredentialtoadd.title')
+      }}>
+      <View style={styles.wrapper}>
+        <ListItemHeader
+          label={t(
+            'wallet:addcredential.choosecredentialtoadd.headers.documents'
+          )}
+        />
+        <VStack space={8}>
+          {availableCredentials.map((credential, index) => {
+            const iconType = getCredentialIconByType(credential);
+            if (iconType) {
+              return (
+                <ModuleCredential
+                  key={credential + '_' + String(index)}
+                  icon={iconType!}
+                  label={getCredentialNameByType(credential)}
+                  onPress={() => {}}
+                  badge={
+                    credential === wellKnownCredential.PID
+                      ? {text: 'Saved', variant: 'success'}
+                      : undefined
+                  }
                 />
-                <VStack space={8}>
-                    {
-                        availableCredentials.map((credential, index) => {
-                            const iconType = getCredentialIconByType(credential);
-                            if (iconType) {
-                                return (
-                                    <ModuleCredential
-                                        key={credential + '_' + String(index)}
-                                        icon={iconType!}
-                                        label={getCredentialNameByType(credential)}
-                                        onPress={() => {}}
-                                        badge={credential === wellKnownCredential.PID ? {text : "Saved", variant : 'success' } : undefined}
-                                    />
-                                );
-                            }
-                            return <></>;
-                        })
-                    }
-                </VStack>
-            </View>
-        </IOScrollViewWithLargeHeader>
-    );
+              );
+            }
+            return <></>;
+          })}
+        </VStack>
+      </View>
+    </IOScrollViewWithLargeHeader>
+  );
 };
 
 const styles = StyleSheet.create({
