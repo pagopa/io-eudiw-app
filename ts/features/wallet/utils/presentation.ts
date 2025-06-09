85651715ee998a0ca3935fb446c61c9c2d6ad39c
eudiwtech <eudiwtech@pagopa.it>
2025-06-09 15:55:32 +0200

    feat: [WLEO-409] Added DCQL support and introduced HealthID render fixes (#84)
    ## Short description
    
    This PR implements new features introduced with `io-react-native-wallet
    v1.7.0`, fixing some _HealthID_ rendering issues and adding _DCQL_
    support.
    
    ## List of changes proposed in this pull request
    - `package.json` , `yarn.lock` : Bumped `io-react-native-wallet` to
    `1.7.0`.
    - `ts/features/wallet/(saga/presentation.ts | utils/presentation.ts)` :
    Added _DCQL_ support by generalizing the last part of the presentation
    flow, now the parsing of the request object and creation of the response
    depend on the `RequestObject` kind (_Presentation Definition_ or
    _DCQL),_ most of the control of the screen router remains in the
    _"main"_ body.
    
    -
    `ts/features/wallet/(components/presentation/PresentationClaimsList.tsx
    | screens/credentialIssuance/CredentialTrust.tsx |
    store/presentation.ts)` : unified _DCQL_ and _Presentation Definition_
    interfaces by reducing the _Descriptor_ type to the strictly required
    properties.
    
    ## How to test
    
    0. Start from a clean installation.
    1. Obtain a PID, an mDL, and an HealthID and then open them: the process
    **should** succeed and there **shouldn't** be any graphical issues in
    the rendering of the credential.
    2. Try presentations to various known-to-be-compatible RPs supporting at
    least one of the _Presentation Definition_ and _DCQL_ methods. The
    presentations **should** succeed.
    a. Try to cancel a presentation to an error-response-supporting RP, the
    RP portal **should** show an `access_denied` message.

---
+++ ts/features/wallet/utils/presentation.ts
@@ -0,0 +1,222 @@
+import {Credential} from '@pagopa/io-react-native-wallet';
+import {call, put} from 'typed-redux-saga';
+import {
+  AuthResponse,
+  OptionalClaims,
+  setPreDefinitionSuccess
+} from '../store/presentation';
+
+type DcqlQuery = Parameters<Credential.Presentation.EvaluateDcqlQuery>[0];
+export type RequestObject = Awaited<
+  ReturnType<Credential.Presentation.VerifyRequestObjectSignature>
+>['requestObject'];
+export type JWK = Awaited<
+  ReturnType<typeof Credential.Presentation.fetchJwksFromRequestObject>
+>['keys'][0];
+
+/**
+ * This generator function must parse the {@link RequestObject}, extract the disclosures
+ * and {@link put} them in an action that drives the router to a proper selection screen.
+ * @param requestObject : The RP presented {@link RequestObject}
+ * @param credentialsSdJwt : An array of string triples representing, in order,
+ *                           the credential type, the credential claim key and the corresponding value
+ * @param credentialsMdoc  : An array of string triples representing, in order,
+ *                           the credential type, the credential claim key and the corresponding value
+ * @returns A {@link Generator} that, after progressing the Presentation saga, returns an object describing which
+ * disclosures to share with the RP to be processed by a compatible {@link PresentationResponseProcessor}
+ */
+export type PresentationRequestProcessor<T> = (
+  requestObject: RequestObject,
+  credentialsSdJwt: Array<[string, string, string]>,
+  credentialsMdoc: Array<[string, string, string]>
+) => Generator<unknown, T, any>;
+
+/**
+ * This generator function handles the generation of the proper {@link AuthResponse} to send
+ * to the RP by combining the required disclosures parsed by the {@link PresentationRequestProcessor}
+ * and the optional disclosures chosen by the user.
+ * @param toProcess : The return value of a corresponding {@link PresentationRequestProcessor} invocation
+ * @param requestObject : The RP presented {@link RequestObject}
+ * @param optionalClaims : The array of optional disclosures chosen by the user
+ * @param jwks : An Array of {@link JWK}s used to create the {@link AuthResponse}
+ * @returns A {@link Generator} that creates the Presentation response for the RP and returns its {@link AuthResponse}
+ */
+export type PresentationResponseProcessor<T> = (
+  toProcess: T,
+  requestObject: RequestObject,
+  optionalClaims: Array<OptionalClaims>,
+  jwks: Array<JWK>
+) => Generator<unknown, AuthResponse, any>;
+
+type EvaluateDcqlReturn = Awaited<
+  ReturnType<typeof Credential.Presentation.evaluateDcqlQuery>
+>;
+type EvaluatePresentationDefinitionReturn = {
+  inputDescriptors: Awaited<
+    ReturnType<typeof Credential.Presentation.evaluateInputDescriptors>
+  >;
+  presentationDefinitionId: string;
+};
+
+/**
+ * Helper method to parse a DCQL request
+ */
+export const handleDcqlRequest: PresentationRequestProcessor<EvaluateDcqlReturn> =
+  function* (
+    requestObject: RequestObject,
+    credentialsSdJwt: Array<[string, string, string]>,
+    credentialsMdoc: Array<[string, string, string]>
+  ) {
+    const evaluateDcqlQuery = yield* call(
+      Credential.Presentation.evaluateDcqlQuery,
+      requestObject.dcql_query as DcqlQuery,
+      credentialsSdJwt,
+      credentialsMdoc
+    );
+
+    yield* put(
+      setPreDefinitionSuccess(
+        evaluateDcqlQuery.map(query => ({
+          requiredDisclosures: query.requiredDisclosures,
+          optionalDisclosures: []
+        }))
+      )
+    );
+
+    return evaluateDcqlQuery;
+  };
+
+/**
+ * Helper method to send a DCQL response
+ */
+export const handleDcqlResponse: PresentationResponseProcessor<EvaluateDcqlReturn> =
+  function* (
+    toProcess: EvaluateDcqlReturn,
+    requestObject: RequestObject,
+    _: Array<OptionalClaims>,
+    jwks: Array<JWK>
+  ) {
+    const credentialsToPresent = toProcess.map(
+      ({requiredDisclosures, ...rest}) => ({
+        ...rest,
+        credentialInputId: rest.id,
+        requestedClaims: requiredDisclosures
+      })
+    );
+
+    const authRequestObject = {
+      nonce: requestObject.nonce,
+      clientId: requestObject.client_id,
+      responseUri: requestObject.response_uri
+    };
+
+    const remotePresentations = yield* call(
+      Credential.Presentation.prepareRemotePresentations,
+      credentialsToPresent,
+      authRequestObject
+    );
+
+    return yield* call(
+      Credential.Presentation.sendAuthorizationResponseDcql,
+      requestObject,
+      jwks,
+      remotePresentations
+    );
+  };
+
+/**
+ * Helper method to prepare and a presentation definition request
+ */
+export const handlePresentationDefinitionRequest: PresentationRequestProcessor<EvaluatePresentationDefinitionReturn> =
+  function* (
+    requestObject: RequestObject,
+    credentialsSdJwt: Array<[string, string, string]>,
+    credentialsMdoc: Array<[string, string, string]>
+  ) {
+    const {presentationDefinition} = yield* call(
+      Credential.Presentation.fetchPresentDefinition,
+      requestObject
+    );
+
+    const evaluateInputDescriptors = yield* call(
+      Credential.Presentation.evaluateInputDescriptors,
+      presentationDefinition.input_descriptors,
+      credentialsSdJwt,
+      credentialsMdoc
+    );
+
+    yield* put(
+      setPreDefinitionSuccess(
+        evaluateInputDescriptors.map(
+          descriptor => descriptor.evaluatedDisclosure
+        )
+      )
+    );
+
+    return {
+      inputDescriptors: evaluateInputDescriptors,
+      presentationDefinitionId: presentationDefinition.id
+    };
+  };
+
+/**
+ * Helper method to send a DCQL response
+ */
+export const handlePresentationDefinitionResponse: PresentationResponseProcessor<EvaluatePresentationDefinitionReturn> =
+  function* (
+    toProcess: EvaluatePresentationDefinitionReturn,
+    requestObject: RequestObject,
+    optionalClaims: Array<OptionalClaims>,
+    jwks: Array<JWK>
+  ) {
+    const credentialAndInputDescriptor = toProcess.inputDescriptors.map(
+      evaluateInputDescriptor => {
+        // Present only the mandatory claims
+        const format = Object.keys(
+          evaluateInputDescriptor.inputDescriptor.format || {}
+        )[0]! as 'mso_mdoc' | 'vc+sd-jwt';
+
+        const requestedClaims = [
+          ...evaluateInputDescriptor.evaluatedDisclosure.requiredDisclosures,
+          ...optionalClaims
+        ];
+
+        return format === 'mso_mdoc'
+          ? {
+              requestedClaims,
+              credentialInputId: evaluateInputDescriptor.inputDescriptor.id,
+              credential: evaluateInputDescriptor.credential,
+              keyTag: evaluateInputDescriptor.keyTag,
+              format,
+              doctype: evaluateInputDescriptor.inputDescriptor.id
+            }
+          : {
+              requestedClaims,
+              credentialInputId: evaluateInputDescriptor.inputDescriptor.id,
+              credential: evaluateInputDescriptor.credential,
+              keyTag: evaluateInputDescriptor.keyTag,
+              format
+            };
+      }
+    );
+
+    const authRequestObject = {
+      nonce: requestObject.nonce,
+      clientId: requestObject.client_id,
+      responseUri: requestObject.response_uri
+    };
+
+    const remotePresentations = yield* call(
+      Credential.Presentation.prepareRemotePresentations,
+      credentialAndInputDescriptor,
+      authRequestObject
+    );
+
+    return yield* call(
+      Credential.Presentation.sendAuthorizationResponse,
+      requestObject,
+      toProcess.presentationDefinitionId,
+      jwks,
+      remotePresentations
+    );
+  };
