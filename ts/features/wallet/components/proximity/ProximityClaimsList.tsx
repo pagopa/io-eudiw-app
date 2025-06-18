bc69fcbd74454379ee144522f54a5673779f283e
eudiwtech <eudiwtech@pagopa.it>
2025-06-18 17:48:36 +0200

    refactor: [WLEO-425] refactored ProximityClaimsList component to make attributes' parser more readable

--- ts/features/wallet/components/proximity/ProximityClaimsList.tsx
+++ ts/features/wallet/components/proximity/ProximityClaimsList.tsx
@@ -20,6 +20,14 @@ type ProximityClaimsListProps = {
   setCheckState: Dispatch<SetStateAction<AcceptedFields>>;
 };
 
+type AttributeDescriptor = {
+  label: string;
+  value: unknown;
+  id: string;
+  toggle: () => void;
+  active: boolean;
+};
+
 /**
  * This component renders the requested disclosures in the {@link ProximityDisclosureDescriptor} by flattening the credential namespaces,
  * and manages a passed {@link AcceptedFields} state which will then be presented to the verifier
@@ -56,43 +64,73 @@ const ProximityClaimsList = ({
   checkState,
   setCheckState
 }: ProximityClaimsListProps) => {
-  const disclosuresViewModel = Object.fromEntries(
-    Object.entries(descriptor).map(([credentialtype, namespaces]) => [
-      getCredentialNameByType(credentialtype),
-      Object.fromEntries(
-        Object.entries(namespaces).flatMap(([namespace, attributes]) =>
-          Object.entries(attributes).map(([attributeName, attribute]) => {
-            const attributeLabel =
-              typeof attribute.name === 'string'
-                ? attribute.name
-                : attribute.name?.[getClaimsFullLocale()] || attributeName;
+  const disclosuresViewModel: Record<
+    string,
+    Record<string, Record<string, AttributeDescriptor>>
+  > = _.mapValues(
+    /**
+     * For each credential type...
+     */
+    descriptor,
+    (namespaces, credentialtype) =>
+      _.mapValues(namespaces, (attributes, namespace) =>
+        _.mapValues(attributes, (attribute, attributeName) => {
+          const attributeLabel =
+            typeof attribute.name === 'string'
+              ? attribute.name
+              : _.get(
+                  attribute,
+                  ['name', getClaimsFullLocale()],
+                  attributeName
+                );
 
-            return [
-              attributeName,
-              {
-                label: attributeLabel,
-                value: attribute.value,
-                id: attributeName,
-                toggle: () => {
-                  // Create a clone of checkState that has the value corresponding to the toggled
-                  // attribute path toggled
-                  const path = [credentialtype, namespace, attributeName];
-                  const newState = _.cloneDeep(checkState);
-                  _.set(newState, path, !_.get(checkState, path));
-                  setCheckState(newState);
-                },
-                active: checkState[credentialtype][namespace][attributeName]
-              }
-            ];
-          })
-        )
+          const path = [credentialtype, namespace, attributeName];
+
+          /**
+           * We transform each attribute's parsed value into an {@link AttributeDescriptor} ...
+           */
+          return {
+            label: attributeLabel,
+            value: attribute.value,
+            id: attributeName,
+            /**
+             * The toggle function inverts the value corresponding to the attribute in
+             * the {@link AcceptedFields} checkState
+             */
+            toggle: () => {
+              const newState = _.cloneDeep(checkState);
+              _.set(newState, path, !_.get(checkState, path));
+              setCheckState(newState);
+            },
+            active: _.get(checkState, path, false)
+          };
+        })
       )
-    ])
+  );
+
+  /**
+   * Then we flatten the attributes of the namespaces (see function's javadoc)
+   */
+  const flattenedDisclosuresViewModel: Record<
+    string,
+    Record<string, AttributeDescriptor>
+  > = _.mapValues(disclosuresViewModel, namespaces =>
+    _.reduce(namespaces, (acc, attributes) => ({...acc, ...attributes}), {})
+  );
+
+  /**
+   * Finally, we remap the credentialTypes to their names
+   */
+  const finalViewModel: Record<
+    string,
+    Record<string, AttributeDescriptor>
+  > = _.mapKeys(flattenedDisclosuresViewModel, (_value, credentialtype) =>
+    getCredentialNameByType(credentialtype)
   );
 
   return (
     <View style={styles.container}>
-      {Object.entries(disclosuresViewModel).map(([entry, attributes]) => (
+      {Object.entries(finalViewModel).map(([entry, attributes]) => (
         <View key={entry} style={styles.credentialContainer}>
           <RawAccordion header={<H4>{entry}</H4>}>
             <View style={styles.claimContainer}>
