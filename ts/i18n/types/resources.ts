e9c80672bdc1a5919e29981c678e9ac483d9b537
LazyAfternoons <LazyAfternoons@users.noreply.github.com>
2025-12-23 14:42:57 +0100

    refactor: [WLEO-738] Use toc for generating translation resources (#110)
    ## Short description
    This PR uses the toc parameter when generating translation resources.
    This allows adding strings at runtime without the need to regenerate the
    resources with the `yarn locales` command. It now needs to be run only
    when adding or removing a namespace.
    
    ## List of changes proposed in this pull request
    - Update the resource script;
    - Remove the old resources file.
    
    ## How to test
    Static checks.

---
+++ ts/i18n/types/resources.ts
@@ -0,0 +1,13 @@
+import global from '../../../locales/en/global.json';
+import onboarding from '../../../locales/en/onboarding.json';
+import qrcodeScan from '../../../locales/en/qrcodeScan.json';
+import wallet from '../../../locales/en/wallet.json';
+
+const resources = {
+  global,
+  onboarding,
+  qrcodeScan,
+  wallet
+} as const;
+
+export default resources;
