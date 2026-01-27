8b544fd55bb435873331b34b3cfa04b9880bed7f
LazyAfternoons <lazyafternoons@outlook.it>
2026-01-27 22:49:03 +0100

    refactor: migrate to expo-application

--- ts/utils/device.ts
+++ ts/utils/device.ts
@@ -1,15 +1,12 @@
+import * as Application from 'expo-application';
 import { Platform } from 'react-native';
-import DeviceInfo from 'react-native-device-info';
 
 /**
  * Returns the application version.
  * @returns a string representing the application version
  */
 export const getAppVersion = () =>
-  Platform.select({
-    ios: DeviceInfo.getReadableVersion(),
-    default: DeviceInfo.getVersion()
-  });
+  `${Application.nativeApplicationVersion} (${Application.nativeBuildVersion})`;
 
 export const isAndroid = Platform.OS === 'android';
 export const isIos = Platform.OS === 'ios';
