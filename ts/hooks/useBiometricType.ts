b1b8cb99667f7c1a1308055ecba37c62a761732f
LazyAfternoons <lazyafternoons@outlook.it>
2026-02-04 15:50:24 +0100

    fix: android not detecting fingerprint removal

--- ts/hooks/useBiometricType.ts
+++ ts/hooks/useBiometricType.ts
@@ -5,52 +5,33 @@ import { selectIsBiometricEnabled } from '../store/reducers/preferences';
 
 export const useBiometricType = () => {
   const isBiometricEnabled = useAppSelector(selectIsBiometricEnabled);
-
   const [biometricType, setBiometricType] = useState<
     LocalAuthentication.AuthenticationType | undefined
   >(undefined);
 
-  const [hasHardware, setHasHardware] = useState<boolean>(false);
-
   useEffect(() => {
-    if (!isBiometricEnabled) {
-      setBiometricType(undefined);
-      setHasHardware(false);
-      return;
-    }
-
     const checkBiometrics = async () => {
-      try {
-        const [hardwareSupported, enrolledLevel, supportedTypes] =
-          await Promise.all([
-            LocalAuthentication.hasHardwareAsync(),
-            LocalAuthentication.getEnrolledLevelAsync(),
-            LocalAuthentication.supportedAuthenticationTypesAsync()
-          ]);
+      /* In order to understand if biometric authentication is available, the permission has been granted and the user has at least one biometric enrolled,
+       * we must combine multiple data:
+       * - isBiometricEnabled from our redux store (user preference)
+       * - hasHardwareAsync which tells if the device has biometric hardware and on iOS returns false also if the permission has not been granted
+       * - isEnrolledAsync which tells if the user has at least one biometric enrolled
+       * - supportedAuthenticationTypesAsync which tells the available biometric types
+       */
+      const [hasHardware, isEnrolled] = await Promise.all([
+        LocalAuthentication.hasHardwareAsync(),
+        LocalAuthentication.isEnrolledAsync()
+      ]);
 
-        setHasHardware(hardwareSupported);
-
-        if (
-          hardwareSupported &&
-          enrolledLevel !== LocalAuthentication.SecurityLevel.NONE &&
-          supportedTypes.length > 0
-        ) {
-          setBiometricType(supportedTypes[0]);
-        } else {
-          setBiometricType(undefined);
-        }
-      } catch {
-        setHasHardware(false);
-        setBiometricType(undefined);
+      if (isBiometricEnabled && hasHardware && isEnrolled) {
+        const types =
+          await LocalAuthentication.supportedAuthenticationTypesAsync();
+        setBiometricType(types.length > 0 ? types[0] : undefined);
       }
     };
 
-    void checkBiometrics();
+    checkBiometrics().catch(() => setBiometricType(undefined));
   }, [isBiometricEnabled]);
 
-  return {
-    biometricType,
-    isBiometricEnabled,
-    hasHardware
-  };
+  return { biometricType };
 };
