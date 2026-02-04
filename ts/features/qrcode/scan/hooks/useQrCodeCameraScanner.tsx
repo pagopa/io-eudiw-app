88b6a05b857f7f99240f67dc14d0296ee3f98427
LazyAfternoons <lazyafternoons@outlook.it>
2026-02-04 23:18:33 +0100

    chore: remove unwanted changes

--- ts/features/qrcode/scan/hooks/useQrCodeCameraScanner.tsx
+++ ts/features/qrcode/scan/hooks/useQrCodeCameraScanner.tsx
@@ -90,6 +90,7 @@ export const useQrCodeCameraScanner = ({
       // After a scan (even if not successful) the decoding is disabled for a while
       // to avoid multiple scans of the same barcode
       setIsResting(true);
+
       scannerReactivateTimeoutHandler.current = setTimeout(() => {
         setIsResting(false);
       }, QRCODE_SCANNER_REACTIVATION_TIME_MS);
